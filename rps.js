1df8d0810fec334bf07d84028de31845fe1ba90d
Douglas Pickett <robertdpickett@gmail.com>
2022-12-19 19:49:27 -0500

    added pseudocode and rps.js

---
+++ rps.js
@@ -0,0 +1,21 @@
+//function getComputerChoice
+//this function assigns a random value of rock,paper,or scissors to the Computer each time
+//input playerSelection (prompt user to select a choice and save this); needs to be case insensitive
+//compare the computerSelection to the playerSelection 
+//depending on comparison return the following the string values
+//if player selection = paper AND computer selection = paper: Tie! Try again!; computer selection = scissors: You lose! Scissors cut paper!; computer selection = rock, You win!. Paper covers rock!
+// if player selection = rock AND computer selection = rock: Tie! Try again!; computer selection = scissors: You win! Rock smashes Scissors!; computer selection = paper, You lose!. Paper covers rock!
+//if player selection = scissors AND computer selection = scissors: Tie! Try again!; computer selection = paper: You win! Scissors cut paper!; computer selection = rock, You lose!. Rock smashes scissors!
+//return this function; do not use console.log()
+
+//playRound function function playRound(playerSelection, computerSelection) {
+  // your code here!
+//}
+ 
+//const playerSelection = "rock";
+//const computerSelection = getComputerChoice();
+
+//write a function game() to call the playROund function and inclue a loop function
+//for (let i = 0; i < 5; i++) {
+   // your code here!
+//}
